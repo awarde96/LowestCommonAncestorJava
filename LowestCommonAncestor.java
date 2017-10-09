3c47e05e14c07426c588c836101b38c959455643
Adam <awarde@tcd.ie>
2017-10-09 17:52:01 +0100

    Added empty source file for code and file for unit tests

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,13 @@
+
+public class LowestCommonAncestor {
+
+	public String helloWorld(){
+		return "HelloWorld";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LowestCommonAncestorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LowestCommonAncestorTest {
+
+	@Test
+	public void test() {
+		LowestCommonAncestor test = new LowestCommonAncestor();
+		assertEquals("First Test", "HelloWorld", test.helloWorld());;
+	}
+
+}
